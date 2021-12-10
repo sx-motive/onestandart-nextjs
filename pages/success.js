94bd6ab6f0728680f484056732c1346ab690c9b4
sx-motive <sx.motive@gmail.com>
2021-12-10 13:49:26 +0500

    form fix

--- pages/success.js
+++ pages/success.js
@@ -1,20 +1,27 @@
-import Link from "next/link"
+import Link from "next/link";
 
-export default function Home() {
-    return (
+export default function Success() {
+  return (
     <section data-scroll-section className="success">
-    <div className="container">
-    <h4>Спасибо, ваша заявка отправлена!</h4>
-    <p>Мы уже обрабатываем вашу заявку, наши менеджеры скоро свяжуться с вами для уточнения данных.</p>
-    <Link href="/">
-    <a data-magnetic className="btn -skew -outline -form" cursor="-color-white">
-        <span data-text="Вернуться на главную">Вернуться на главную</span>
-        <div className="ripple">
-            <div className="ripple-span"></div>
-        </div>
-    </a>
-    </Link>
-    </div>
+      <div className="container">
+        <h4>Спасибо, ваша заявка отправлена!</h4>
+        <p>
+          Мы уже обрабатываем вашу заявку, наши менеджеры скоро свяжуться с вами
+          для уточнения данных.
+        </p>
+        <Link href="/">
+          <a
+            data-magnetic
+            className="btn -skew -outline -form"
+            cursor="-color-white"
+          >
+            <span data-text="Вернуться на главную">Вернуться на главную</span>
+            <div className="ripple">
+              <div className="ripple-span"></div>
+            </div>
+          </a>
+        </Link>
+      </div>
     </section>
-    )
-  }
+  );
+}
